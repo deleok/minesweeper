94627aece3489af9ce72d8928cf999bab20250b2
Karen Zhu <qibaiz@uw.edu>
2021-01-29 20:49:03 -0800

    add docstring

--- input_generating.py
+++ input_generating.py
@@ -1,9 +1,23 @@
+"""
+Qibai Zhu
+
+This input_generating file is used to generate the specified number of mine fields,
+and write into the minesweeper_input.txt file.
+"""
+
 import random
 
 
 class GenerateField:
+    """
+    Generate one field with random size of row and column, placing random number of mines in the field
+    """
 
     def __init__(self):
+        """
+        Generate a field with random row_size and random col_size, both range from (0, 100], exclusive 0, inclusive 100;
+        The number of mine is also randomly generated.
+        """
         self.__row_size = random.randint(3, 5)
         self.__col_size = random.randint(2, 5)
         self.__mine_number = random.randint(2, self.__row_size * self.__col_size // 2)
@@ -13,13 +27,21 @@ class GenerateField:
             self.__field.append(['' for _ in range(0, self.__col_size)])
 
     def get_row_size(self):
+        """
+        Get the row size of the field
+        """
         return self.__row_size
 
     def get_col_size(self):
+        """
+        Get the column size of the field
+        """
         return self.__col_size
 
     def place_mine(self):
-
+        """
+        Randomly place the mines '*' in the field
+        """
         while self.__mine_number > 0:
             row = random.randint(0, self.__row_size - 1)
             col = random.randint(0, self.__col_size - 1)
@@ -29,12 +51,18 @@ class GenerateField:
                 self.__mine_number -= 1
 
     def draw_mine_field(self):
+        """
+        Draw the safe square of the mine field, placing '.' in the square
+        """
         for row in range(0, self.__row_size):
             for col in range(0, self.__col_size):
                 if self.__field[row][col] != '*':
                     self.__field[row][col] = '.'
 
     def print_field(self):
+        """
+        Print the mine field
+        """
         return self.__str__()
 
     def __str__(self):
@@ -42,6 +70,11 @@ class GenerateField:
 
 
 def generate_input(number_of_fields):
+    """
+    Generate specified number of mine fields, write into the minesweeper_input.txt file
+    :param number_of_fields: the number of fields that want to generate
+    :return:
+    """
     while number_of_fields > 0:
         field = GenerateField()
 
@@ -63,4 +96,4 @@ def generate_input(number_of_fields):
 if __name__ == '__main__':
     with open("minesweeper_input.txt", "w"):
         pass
-    generate_input(2)
+    generate_input(3)
